9a6711ef638f37bf0eda8f7bafa4c6a3e508add1
Samuel Ezedi <samuelezedi@gmail.com>
2020-07-10 11:31:37 +0100

    building paint

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,15 @@ class Signature extends CustomPainter {
   List<Offset> points;
   @override
   void paint (Canvas canvas, Size size) {
+
+    Paint paint = Paint()
+    ..color = Colors.black
+    ..strokeCap = StrokeCap.round
+    ..strokeWidth = 5;
+
     for(int i = 0; i < points.length-1; i++) {
       if(points[i] != null && points[i+1] != null) {
-
+        canvas.drawLine(points[i], points[i+1], paint);
       }
     }
   }
